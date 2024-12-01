3bed96b043e628766f147de645ff70286aa8ede4
xkwdx <mainrxdisxj@gmail.com>
2024-12-01 12:40:02 +0100

    first commit

---
+++ js/rejestracja.js
@@ -0,0 +1,4 @@
+document.getElementById('formularzrejestracji').addEventListener('submit', function (zdarzenie) {
+    zdarzenie.preventDefault();
+    alert('Rejestracja zakończona pomyślnie!');
+});
---
+++ js/rezerwacja.js
@@ -0,0 +1,4 @@
+document.getElementById('formularzrezerwacji').addEventListener('submit', function (zdarzenie) {
+    zdarzenie.preventDefault();
+    alert('Wizyta została zarezerwowana!');
+});
